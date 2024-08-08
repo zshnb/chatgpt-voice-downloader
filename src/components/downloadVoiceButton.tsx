9634363dda44e2661565429e380db31f74020f38
zshnb <a857681664@gmail.com>
2024-08-08 22:50:27 +0800

    add download icon, handle download

--- src/components/downloadVoiceButton.tsx
+++ src/components/downloadVoiceButton.tsx
@@ -1,29 +1,46 @@
 import {sendToBackground} from "@plasmohq/messaging";
-import type {MouseEvent} from "react";
-import {Button, CircularProgress} from "@nextui-org/react";
+import {type MouseEvent, useState} from "react";
+import {Button} from "@nextui-org/react";
+import {Download} from "lucide-react";
 
-export type DownloadVoiceButtonProps = {
-  messageId: string
-}
 const DownloadVoiceButton = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   async function handleDownload(event: MouseEvent<HTMLButtonElement>) {
-    // @ts-ignore
-    const groupDom = event.target.offsetParent.parentElement.offsetParent
-    const messageDom = groupDom.querySelector('div[data-message-id]')
-    console.log(messageDom)
-    const messageId = messageDom.getAttribute('data-message-id')
-    await sendToBackground({
-      name: 'download',
-      body: {
-        messageId
-      },
-      extensionId: 'olcjfanhelecheolfimeglloebiknnjb'
-    })
+    function getGroupParentDom() {
+      // @ts-ignore
+      if (event.target.nodeName === 'svg') {
+        // @ts-ignore
+        return event.target.parentElement.offsetParent.parentElement.offsetParent
+        // @ts-ignore
+      } else if (event.target.nodeName === 'line') {
+        // @ts-ignore
+        return event.target.parentElement.parentElement.offsetParent.parentElement.offsetParent
+      } else {
+        // @ts-ignore
+        return event.target.offsetParent.parentElement.offsetParent
+      }
+    }
+    setLoading(true)
+    try {
+      // @ts-ignore
+      const groupDom = getGroupParentDom()
+      const messageDom = groupDom.querySelector('div[data-message-id]')
+      const messageId = messageDom.getAttribute('data-message-id')
+      await sendToBackground({
+        name: 'download',
+        body: {
+          messageId
+        },
+        extensionId: 'olcjfanhelecheolfimeglloebiknnjb'
+      })
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
-    <Button color="danger" isLoading>
-      124
+    <Button color="danger" isLoading={loading} onClick={handleDownload} className={'p-0 min-w-4'}>
+      <Download size={14}/>
     </Button>
   )
 }
