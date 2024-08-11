58f40314f87feb7e99f462e5f0b5ea34e434024d
Sleek Zheng <slick.zheng@opus.pro>
2024-08-11 15:54:09 +0800

    feat: integrate nextui, add icon on download button (#1)
    * adjust download button position
    
    * nextui
    
    * add download icon, handle download
    
    * adjust loading style

--- src/components/downloadVoiceButton.tsx
+++ src/components/downloadVoiceButton.tsx
@@ -1,27 +1,53 @@
 import {sendToBackground} from "@plasmohq/messaging";
-import type {MouseEvent} from "react";
+import {type MouseEvent, useState} from "react";
+import {Button, Spinner} from "@nextui-org/react";
+import {Download} from "lucide-react";
 
-export type DownloadVoiceButtonProps = {
-  messageId: string
-}
-const DownloadVoiceButton = ({messageId}: DownloadVoiceButtonProps) => {
+const DownloadVoiceButton = () => {
+  const [loading, setLoading] = useState<boolean>(false);
   async function handleDownload(event: MouseEvent<HTMLButtonElement>) {
-    // @ts-ignore
-    const groupDom = event.target.offsetParent.parentElement.offsetParent
-    const messageDom = groupDom.querySelector('div[data-message-id]')
-    console.log(messageDom)
-    const messageId = messageDom.getAttribute('data-message-id')
-    await sendToBackground({
-      name: 'download',
-      body: {
-        messageId
-      },
-      extensionId: 'olcjfanhelecheolfimeglloebiknnjb'
-    })
+    function getGroupParentDom() {
+      console.log(event)
+      // @ts-ignore
+      if (event.target.nodeName === 'svg') {
+        // @ts-ignore
+        return event.target.parentElement.offsetParent.parentElement.offsetParent
+        // @ts-ignore
+      } else if (event.target.nodeName === 'line') {
+        // @ts-ignore
+        return event.target.parentElement.parentElement.offsetParent.parentElement.offsetParent
+      } else {
+        // @ts-ignore
+        return event.target.offsetParent.parentElement.offsetParent
+      }
+    }
+    setLoading(true)
+    try {
+      // @ts-ignore
+      const groupDom = getGroupParentDom()
+      const messageDom = groupDom.querySelector('div[data-message-id]')
+      const messageId = messageDom.getAttribute('data-message-id')
+      await sendToBackground({
+        name: 'download',
+        body: {
+          messageId
+        },
+        extensionId: 'olcjfanhelecheolfimeglloebiknnjb'
+      })
+    } finally {
+      setLoading(false)
+    }
   }
 
   return (
-    <button className={'plasmo-p-2 plasmo-rounded'} onClick={handleDownload}>下载</button>
+    <Button color="danger" onClick={handleDownload} className={'p-0 min-w-4'}>
+      {loading && <Spinner classNames={{
+        wrapper: 'w-4 h-4',
+        circle1: 'border-2',
+        circle2: 'border-2'
+      }}/>}
+      {!loading && <Download size={14}/>}
+    </Button>
   )
 }
 
