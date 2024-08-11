c179820dee19d42d3c0f79dab737121c953e2d42
zshnb <a857681664@gmail.com>
2024-08-11 16:28:42 +0800

    compete popup page

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,8 +1,28 @@
 import "~style.css"
+import {Button, Select, SelectItem} from "@nextui-org/react";
+import {useState} from "react";
 
 function IndexPopup() {
+  const voices = ['breeze', 'juniper', 'ember', 'cove']
+  const [voice, setVoice] = useState('breeze')
   return (
-    <div className="plasmo-flex plasmo-items-center plasmo-justify-center plasmo-h-16 plasmo-w-40">
+    <div className="w-96 h-96 p-10">
+      <form className={'flex flex-col gap-y-2 relative'}>
+        <h1 className={'text-center text-xl'}>ChatGPT音频下载器</h1>
+        <Select
+          placeholder="选择声音"
+          className="max-w-xs"
+          selectedKeys={[voice]}
+          onChange={(e) => setVoice(e.target.value)}
+        >
+          {voices.map((voice) => (
+            <SelectItem key={voice}>
+              {voice}
+            </SelectItem>
+          ))}
+        </Select>
+        <Button color={'primary'} className={''}>下载所有音频</Button>
+      </form>
     </div>
   )
 }
